0d8f9f45a84ff07e645a7e43b8633c8076d1480a
David Kramer <david.kramer@redtailtechnology.com>
2019-06-10 17:40:04 -0700

    wip

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component, KeyboardEvent } from 'react';
+import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import localforage from 'localforage'
